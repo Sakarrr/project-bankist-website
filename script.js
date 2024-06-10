19735a20c2f8ae4baba24e623a35fabeeb17c7fa
Sakarr <ctha.sakar@gmail.com>
2024-06-10 17:40:03 +0545

    Implement - Lazy loading

--- script.js
+++ script.js
@@ -104,8 +104,9 @@ btnScrollTo.addEventListener('click', function (e) {
 // });
 
 tabsContainer.addEventListener('click', function (e) {
+  console.log(e.target);
   const clicked = e.target.closest('.operations__tab');
-  // console.log(clicked);
+  console.log(clicked);
 
   // Guard Clause.
   if (!clicked) return;
@@ -193,3 +194,31 @@ allSections.forEach(function (section) {
   sectionObserver.observe(section);
   section.classList.add('section--hidden');
 });
+
+// Lazy loading images
+const imgTargets = document.querySelectorAll('img[data-src]');
+console.log(imgTargets);
+
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  // Replace src with data-src
+  entry.target.src = entry.target.dataset.src;
+  // entry.target.classList.remove('lazy-img'); // Not good approach on slow devices
+
+  entry.target.addEventListener('load', function () {
+    entry.target.classList.remove('lazy-img');
+  });
+
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: '200px',
+});
+
+imgTargets.forEach(img => imgObserver.observe(img));
